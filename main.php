26e8fe890f1396b70d48e27db853d1c37f9a2e48
NaotoSuzuki <suzuki.naoto@teamup.jp>
2018-05-13 17:46:10 +0900

    いらないコードを削除

--- main.php
+++ main.php
@@ -1,13 +1,8 @@
 <?php
 
-class UnitTest{
- 
-}
-
 class FizzBuzz {
 }
 
-var_dump(assert(1 == 1, '値が一致しません'));
 $FizzBuzz = new FizzBuzz();
 var_dump(assert($FizzBuzz->input(1) == 1, '値が一致しません'));
 ?>
