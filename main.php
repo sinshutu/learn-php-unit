7a3230a6cd2226f8c34d7e1059dc6131b6156b43
NaotoSuzuki <suzuki.naoto@teamup.jp>
2018-05-13 17:49:51 +0900

    inputの仮実装2をした

--- main.php
+++ main.php
@@ -1,11 +1,12 @@
 <?php
 
 class FizzBuzz {
-    function input() {
-        return 1;
+    function input($x) {
+        return $x;
     }
 }
 
 $FizzBuzz = new FizzBuzz();
 var_dump(assert($FizzBuzz->input(1) == 1, '値が一致しません'));
+var_dump(assert($FizzBuzz->input(2) == 2, '値が一致しません'));
 ?>
