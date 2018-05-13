fb045734a38c60db3dcb4cf2b910a5b7ea6953fe
godan <matsushita@godan.xyz>
2018-05-13 18:15:46 +0900

    [add] minus test

--- main.php
+++ main.php
@@ -18,4 +18,9 @@ var_dump(assert($FizzBuzz->input(5) == 'buzz', '値が一致しません'));
 var_dump(assert($FizzBuzz->input(6) == 'fizz', '値が一致しません'));
 var_dump(assert($FizzBuzz->input(10) == 'buzz', '値が一致しません'));
 var_dump(assert($FizzBuzz->input(15) == 'fizzbuzz', '値が一致しません'));
+
+var_dump(assert($FizzBuzz->input(-1) == '-1', '値が一致しません'));
+var_dump(assert($FizzBuzz->input(-3) == 'fizz', '値が一致しません'));
+var_dump(assert($FizzBuzz->input(-5) == 'buzz', '値が一致しません'));
+var_dump(assert($FizzBuzz->input(-15) == 'fizzbuzz', '値が一致しません'));
 ?>
