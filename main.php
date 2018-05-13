773589acfe0135f14ac96b397902227e6cc38a47
NaotoSuzuki <suzuki.naoto@teamup.jp>
2018-05-13 18:41:02 +0900

    全角対応

--- main.php
+++ main.php
@@ -2,7 +2,8 @@
 
 class FizzBuzz {
     function input($x) {
-        if($x === 0 ) return 'not break';
+        if(is_string($x)) $x = (int)mb_convert_kana($x, "n", "utf-8");
+        if($x === 0) return 'not break';
         if($x % 15 === 0) return 'fizzbuzz';
         if($x % 3 === 0) return 'fizz';
         if($x % 5 === 0) return 'buzz';
@@ -25,5 +26,15 @@ var_dump(assert($FizzBuzz->input(-3) == 'fizz', '値が一致しません'));
 var_dump(assert($FizzBuzz->input(-5) == 'buzz', '値が一致しません'));
 var_dump(assert($FizzBuzz->input(-15) == 'fizzbuzz', '値が一致しません'));
 
-var_dump(assert($FizzBuzz->input(0) == 'not break','値が一致しません'));
+var_dump(assert($FizzBuzz->input(0) == 'not break', '値が一致しません'));
+
+var_dump(assert($FizzBuzz->input('１') == 1,'値が一致しません'));
+var_dump(assert($FizzBuzz->input('２') == 2,'値が一致しません'));
+var_dump(assert($FizzBuzz->input('３') == 'fizz','値が一致しません'));
+var_dump(assert($FizzBuzz->input('４') == 4,'値が一致しません'));
+var_dump(assert($FizzBuzz->input('５') == 'buzz','値が一致しません'));
+var_dump(assert($FizzBuzz->input('６') == 'fizz','値が一致しません'));
+var_dump(assert($FizzBuzz->input('１０') == 'buzz','値が一致しません'));
+var_dump(assert($FizzBuzz->input('１５') == 'fizzbuzz','値が一致しません'));
+var_dump(assert($FizzBuzz->input('０') == 'not break','値が一致しません'));
 ?>
