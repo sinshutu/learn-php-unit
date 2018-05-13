ab48b677f2121e96e73ba1eb161fc8302a96ab19
Oosaki Naoya <noblesse.oblige7@suou.waseda.jp>
2018-05-13 17:44:51 +0900

    test driven devをしたよ！

--- main.php
+++ main.php
@@ -1,8 +1,13 @@
 <?php
+
+class UnitTest{
+ 
+}
+
+class FizzBuzz {
+}
+
 var_dump(assert(1 == 1, '値が一致しません'));
-
-class FizzBazz {
-   
-} 
-
+$FizzBuzz = new FizzBuzz();
+var_dump(assert($FizzBuzz->input(1) == 1, '値が一致しません'));
 ?>
